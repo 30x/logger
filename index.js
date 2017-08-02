e6e92be5a93b31c36ae1e4f4e6af32c057a293f1
Martin Nally <mnally@google.com>
2017-08-01 18:00:21 -0700

    first

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ class Logger {
   constructor(component) { // component is likely to corresponmd to a modeul, but does not have to
     this.component = component;
   }
-  _log(level, functionName, context, message) {
-    console.log(new Date().toISOString(), level, this.module, functionName, context['request-id'], content.user, message)
+  _log(level, functionName, context, ...args) {
+    args.unshift(new Date().toISOString(), level, this.component, functionName, context['request-id'] || 'no-request-id', context.user || 'INCOGNITO')
+    console.log.apply(null, args)
   }
-  info(functionName, context, message) {
-    console.log(new Date().toISOString(), 'info', this.module, functionName, context['request-id'] || 'no-request-id', content.user || 'INCOGNITO', message)
+  info(...args) {
+    args.unshift('info')
+    this._log.apply(this,args)
   }
-  debug(functionName, context, message) {
-    console.log(new Date().toISOString(), 'info', this.module, functionName, context['request-id'], content.user, message)
+  debug(...args) {
+    args.unshift('debug')
+    this._log.apply(this, args)
   }
-  error(functionName, context, message) {
-    console.log(new Date().toISOString(), 'info', this.module, functionName, context['request-id'], content.user, message)
+  error(...args) {
+    args.unshift('\x1b[30;101merror\x1b[0m')
+    this._log.apply(this, args)
   }
 }
+
+exports.Logger = Logger
